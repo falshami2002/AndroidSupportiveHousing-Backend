19f91b443eb1e8084664ba9feb765bed70db36ed
Alshami <falshami@csu.fullerton.edu>
2025-04-10 20:00:11 -0700

    drop again

--- app.js
+++ app.js
@@ -23,10 +23,16 @@ const db = new sqlite3.Database('./database.db', (err) => {
             pill_id INTEGER,
             dispense_time TIME 
         )`);
-        db.run(`CREATE TABLE IF NOT EXISTS pot (
-            recipe_id INTEGER PRIMARY KEY,
+        db.run("DROP TABLE IF EXISTS pot", () => {
+            db.run(`CREATE TABLE pot (
+                recipe_id INTEGER,
+                current_step INTEGER
+            )`);
+        });
+        /*db.run(`CREATE TABLE IF NOT EXISTS pot (
+            recipe_id INTEGER,
             current_step INTEGER
-        )`);
+        )`);*/
         db.run(`CREATE TABLE IF NOT EXISTS recipes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT, 
