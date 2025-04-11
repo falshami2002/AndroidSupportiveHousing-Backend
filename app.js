552961bf48d83e0ecdc3609a88a6e2204daac2e4
Alshami <falshami@csu.fullerton.edu>
2025-04-10 21:40:33 -0700

    Fixed errors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const db = new sqlite3.Database('./database.db', (err) => {
         )`);*/
         db.run("DROP TABLE IF EXISTS recipes", () => {
             db.run(`CREATE TABLE recipes (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id INTEGER PRIMARY KEY,
                 name TEXT, 
                 estimated_time INTEGER,
                 ingredients TEXT
@@ -95,12 +95,13 @@ app.get('/recipe', (req, res) => {
 
 //post recipe
 app.post('/recipe', (req, res) => {
-    const {name, estimated_time, ingredients} = req.body;
-    db.all(`INSERT INTO pot (name, estimated_time, ingredients) VALUES (?, ?, ?)`, [name, estimated_time, JSON.stringify(ingredients)], (err, values) => {
+    const {id, name, estimated_time, ingredients} = req.body;
+    db.all(`INSERT INTO recipes (id, name, estimated_time, ingredients) VALUES (?, ?, ?)`, [id, name, estimated_time, JSON.stringify(ingredients)], (err, values) => {
         if (err) {
             res.status(500).json({ error: err.message });
         } else {
             res.status(200).json({
+                id: id,
                 name: name,
                 estimated_time: estimated_time,
                 ingredients: ingredients
