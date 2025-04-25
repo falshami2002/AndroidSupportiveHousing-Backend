eb44150397bcd547fcedbce3dc41b061d8bd3f05
Alshami <falshami@csu.fullerton.edu>
2025-04-24 18:25:29 -0700

    Resetting table

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ const db = new sqlite3.Database('./database.db', (err) => {
             pill_id INTEGER,
             dispense_time TIME 
         )`);
+        db.run(`DROP TABLE IF EXISTS pot`);
         db.run(`CREATE TABLE IF NOT EXISTS pot (
             recipe_id INTEGER PRIMARY KEY,
             current_step INTEGER
