d26414e3b7863aa13584f52b53d7ab20471b82e3
Komal-7 <kirikomal777@gmail.com>
2025-07-01 23:51:08 -0700

    env var

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,5 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+if (!serviceAccountString) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT env var is missing!");
+}
+
+const serviceAccount = JSON.parse(serviceAccountString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
