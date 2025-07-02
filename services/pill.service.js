674ccf0dbf3fc0fe24a10e571744ac4d1e0bce51
Komal-7 <kirikomal777@gmail.com>
2025-07-01 17:03:30 -0700

    test2

--- services/pill.service.js
+++ services/pill.service.js
@@ -123,10 +123,20 @@ async function sendPushNotificationToUser(deviceId, pill_id, dispense_time) {
     [deviceId]
   );
   console.log("fcm from table",user)
+  db.all(`SELECT * FROM pillDeviceTokens`, (err, values) => {
+        if (err) {
+            // res.status(500).json({ error: err.message });
+            console.log("Err here her here")
+        } else {
+            // res.json(values);
+            console.log("from test",values)
+        }
+    });
   if (!user?.fcm_token) {
     console.warn(`No FCM token for device ${deviceId}`);
     return;
   }
+
 
 //   const notification = {
 //     to: user.fcm_token,
