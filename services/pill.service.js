22afd731ff04b1c125a472ce16dc991e1ec2cddd
Komal-7 <kirikomal777@gmail.com>
2025-07-14 17:04:58 -0700

    minor-fix

--- services/pill.service.js
+++ services/pill.service.js
@@ -75,7 +75,7 @@ exports.addPillSchedule = async (req, res) => {
         return res.status(400).json({ error: "Missing or empty schedules" });
     }
 
-    const payloadToSend = schedules.map(timestamp =>  ({pill_id: timestamp.pill_id, dispense_time: formatTimestamp12Hour(timestamp.dispense_time), pill_slot: entry.pill_slot}));
+    const payloadToSend = schedules.map(timestamp =>  ({pill_id: timestamp.pill_id, dispense_time: formatTimestamp12Hour(timestamp.dispense_time), pill_slot: timestamp.pill_slot}));
 
         console.log("payload to send",payloadToSend)
 
