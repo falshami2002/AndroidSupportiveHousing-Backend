923a2c219583025eaa02e2582f67d1d593ea81b5
Komal-7 <kirikomal777@gmail.com>
2025-07-23 21:08:26 -0700

    get recipe added

--- routes/pot.routes.js
+++ routes/pot.routes.js
@@ -6,5 +6,6 @@ router.get('/current-recipe', potService.getCurrentRecipe);
 router.put('/current-recipe', potService.putCurrentRecipe);
 router.post('/current-recipe', potService.postCurrentRecipe);
 router.delete('/current-recipe', potService.deleteCurrentRecipe);
+router.get('/recipe/:id',potService.getRecipeById);
 
 module.exports = router;
